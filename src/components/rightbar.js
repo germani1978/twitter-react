0e5f86817ec7b4616312b6850291595267ddca6e
germani <germanibenitezalfonso1@gmail.com>
2023-03-10 16:54:12 -0500

    rightbar

--- src/components/rightbar.js
+++ src/components/rightbar.js
@@ -1,5 +1,21 @@
+function PanelBuscar(params) {
+    return <div>
+        <input />
+    </div>
+}
+
+function TemasTrending(params) {
+    return <div>
+        <p>Elon Musk</p>
+        <p>Elon Musk</p>
+        <p>Elon Musk</p>
+        <p>Elon Musk</p>
+        <p>Elon Musk</p>
+        <p>Elon Musk</p>
+    </div>
+}
 export function RightBar(params) {
     return <div>
-        
+        <PanelBuscar/>
     </div>
 }
